0de0d8a8a61b532b1568b97ce7456608f98cde70
Barlow0471 <mattbar2@comcast.net>
2021-09-20 21:27:04 -0400

    starting out

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,7 +1,19 @@
-const buttons = 'samples.json'
+// Use the list of sample names to populate the select options  
+function init() {
+// Grab a reference to the dropdown select element  
+var selector = d3.select("#selDataset");
+d3.json("samples.json").then((data) => {   
+     var sampleNames = data.names;
+    sampleNames.forEach((sample) => {     
+         selector
+         .append("option")        
+         .text(sample)        
+         .property("value", sample);   
+         });
 
-d3.json(buttons).then(function(data) {
-    console.log(data);
-  });
-  
-  
+         // Use the first sample from the list to build the initial plots    
+         var firstSample = sampleNames[0];    
+         buildCharts(firstSample);    
+         buildMetadata(firstSample);  });}
+// Initialize the dashboard
+init();
